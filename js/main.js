610ffed3142c236b89b778a59b5d83e8468b6b7f
Nathan L Smith <nlloyds@gmail.com>
2011-04-27 12:19:44 -0500

    add backbone project setup

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //Configure RequireJS
 require({
-    //Load jQuery before any other scripts, since jQuery plugins normally
-    //assume jQuery is already loaded in the page.
-    priority: ['jquery']
+    // Load jQuery, Underscore, and backbone before any other scripts.
+    priority: ['jquery', 'underscore', 'backbone']
 });
 
-require(['jquery'], function ($) {
+require([
+    "jquery",
+    "underscore",
+    "backbone"
+], function ($) {
 
 });
 
